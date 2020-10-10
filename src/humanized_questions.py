975239ff5ed87ef367b3526958cf434b80eb22cd
Rodrigo Ferrerira Guimarães <rodrigofegui@gmail.com>
2020-10-09 22:38:16 -0300

    Add: Humanized questions to melasma data acquisition

--- src/humanized_questions.py
+++ src/humanized_questions.py
@@ -2,12 +2,33 @@ def ask_about_fiver(medical_record):
 
     print('Poderia nos informa a sua temperatura?')
     resp = float(input().replace(',', '.'))
-    print('\tresp', resp)
     medical_record.input['temperatura'] = resp
 
     print('E por quantos dias?')
     resp = int(input())
-    print('\tresp', resp)
     medical_record.input['duração da febre'] = resp
+
+    return medical_record
+
+
+def ask_about_melasma(medical_record):
+    from random import randint
+    while(True):
+        print('Apareceu algumas manchas na sua pele na última semana? (sim/não)')
+        resp = input()
+
+        if resp == 'não':
+            medical_record.input['melasma'] = 0
+            medical_record.input['melasma_occurence'] = 0
+            return medical_record
+
+        if resp == 'sim':
+            break
+
+    print('Saberia dizer há quantos dias elas apareceram?')
+    resp = 7 - int(input())
+
+    medical_record.input['melasma'] = resp
+    medical_record.input['melasma_occurence'] = randint(0, 10)
 
     return medical_record
--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from skfuzzy import control
 
-from humanized_questions import ask_about_fiver
+from humanized_questions import ask_about_fiver, ask_about_melasma
 from rules import all_rules
 from utils import inform_diagnosis
 
@@ -10,10 +10,12 @@ def run_system():
     medical_record = control.ControlSystemSimulation(aedes_aegypti_diagnosis)
 
     medical_record = ask_about_fiver(medical_record)
+    medical_record = ask_about_melasma(medical_record)
 
     medical_record.compute()
 
     inform_diagnosis(medical_record.output['doenças'])
+
 
 if __name__ == '__main__':
     run_system()
