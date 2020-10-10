d9bd0b58691d636292d0da0d4d14437f780a8e12
Luiz Gustavo <martins.luiz131@gmail.com>
2020-10-10 15:36:17 -0300

    Add: Rules about muscle pain

--- src/consequents.py
+++ src/consequents.py
@@ -15,3 +15,13 @@ def get_melasma_consequent():
     disease['chikungunya'] = fuzzy.trimf(disease.universe, [4, 5, 6])
 
     return disease
+
+
+def get_muscle_pain_consequent():
+    disease = control.Consequent(arange(0, 10, .1), 'from muscle_pain')
+
+    disease['dengue'] = fuzzy.trimf(disease.universe, [0, 1, 1])
+    disease['zika'] = fuzzy.tripmf(disease.universe, [0, 2, 3])
+    disease['chikungunya'] = fuzzy.trimf(disease.universe, [1, 2, 3])
+
+    return disease
