0e78832092ae7be6a559f82d8abb75096f234187
Rodrigo Ferrerira Guimarães <rodrigofegui@gmail.com>
2020-10-09 22:37:23 -0300

    Add: Rules about melasma

--- src/consequents.py
+++ src/consequents.py
@@ -1,6 +1,17 @@
 from numpy import arange
 from skfuzzy import control
+import skfuzzy as fuzzy
 
 disease = control.Consequent(arange(0, 10, 1), 'doenças')
 
 disease.automf(names=['dengue', 'zika', 'chikungunya'])
+
+
+def get_melasma_consequent():
+    disease = control.Consequent(arange(0, 10, .1), 'from melasma')
+
+    disease['dengue'] = fuzzy.trapmf(disease.universe, [2, 3, 5, 6])
+    disease['zika'] = fuzzy.trapmf(disease.universe, [8, 9, 10, 10])
+    disease['chikungunya'] = fuzzy.trimf(disease.universe, [4, 5, 6])
+
+    return disease
--- src/rules.py
+++ src/rules.py
@@ -1,9 +1,8 @@
 from skfuzzy import control
 
-from consequents import disease
-
 
 def get_fiver_rules():
+    from consequents import disease
     from antecedents import get_fiver_antecedents
 
     temperature, duration = get_fiver_antecedents()
@@ -23,7 +22,29 @@ def get_fiver_rules():
         )
     ]
 
+
+def get_melasma_rules():
+    from consequents import disease
+    from antecedents import get_melasma_antecedents
+
+    when_happened, occurence = get_melasma_antecedents()
+
+    return [
+        control.Rule(
+            when_happened['middle'] & occurence['uncommom'],
+            disease['dengue']
+        ),
+        control.Rule(
+            when_happened['beginning'] & occurence['for_sure'],
+            disease['zika']
+        ),
+        control.Rule(
+            when_happened['all'] & occurence['middle'],
+            disease['chikungunya']
+        )
+    ]
+
+
 all_rules = []
-all_rules.extend(
-    get_fiver_rules()
-)
+all_rules.extend(get_fiver_rules())
+all_rules.extend(get_melasma_rules())
