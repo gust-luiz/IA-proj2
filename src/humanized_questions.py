b15042e3c22e52096b5fc8a50be9628742f3eeb1
Luiz Gustavo <martins.luiz131@gmail.com>
2020-10-10 15:36:32 -0300

    Add: Question about muscle pain

--- src/humanized_questions.py
+++ src/humanized_questions.py
@@ -1,6 +1,6 @@
 def ask_about_fiver(medical_record):
 
-    print('Poderia nos informa a sua temperatura?')
+    print('Poderia nos informar a sua temperatura?')
     resp = float(input().replace(',', '.'))
     medical_record.input['temperatura'] = resp
 
@@ -14,7 +14,7 @@ def ask_about_fiver(medical_record):
 def ask_about_melasma(medical_record):
     from random import randint
     while(True):
-        print('Apareceu algumas manchas na sua pele na última semana? (sim/não)')
+        print('Apareceram algumas manchas na sua pele na última semana? (sim/não)')
         resp = input()
 
         if resp == 'não':
@@ -30,5 +30,26 @@ def ask_about_melasma(medical_record):
 
     medical_record.input['melasma'] = resp
     medical_record.input['melasma_occurence'] = randint(0, 10)
+
+    return medical_record
+
+
+def ask_about_muscle_pain(medical_record):
+  
+    while(True):
+        print('Sentiu dor muscular durante a última semana? (sim/não)')
+        resp = input()
+
+        if resp == 'não':
+            medical_record.input['muscle_pain'] = 0
+            return medical_record
+
+        if resp == 'sim':
+            break
+
+    print('Entre 1 (pouco frequente) e 3 (muito frequente), qual foi a frequência com que sentiu dores musculares? (1/2/3)')
+    resp = int(input())
+
+    medical_record.input['muscle_pain'] = resp
 
     return medical_record
