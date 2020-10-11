291e7dbfcb7f90d0873fccd8a5cf465adfca7449
Luiz Gustavo <martins.luiz131@gmail.com>
2020-10-11 17:02:35 -0300

    Add: Antecedents about ganglionic hypertrophy

--- src/antecedents.py
+++ src/antecedents.py
@@ -28,13 +28,13 @@ def get_melasma_antecedents():
 
 
 def get_muscle_pain_antecedents():
-    muscle_pain_frequency = control.Antecedent(arange(0, 10, .1), 'muscle_pain_frequency')
+    frequency = control.Antecedent(arange(0, 10, .1), 'muscle_pain_frequency')
 
-    muscle_pain_frequency['low'] = fuzzy.gaussmf(muscle_pain_frequency.universe, 0, 1.5)
-    muscle_pain_frequency['medium'] = fuzzy.gaussmf(muscle_pain_frequency.universe, 5, .75)
-    muscle_pain_frequency['high'] = fuzzy.gaussmf(muscle_pain_frequency.universe, 10, 1.5)
+    frequency['low'] = fuzzy.gaussmf(frequency.universe, 0, 1.5)
+    frequency['medium'] = fuzzy.gaussmf(frequency.universe, 5, .75)
+    frequency['high'] = fuzzy.gaussmf(frequency.universe, 10, 1.5)
 
-    return muscle_pain_frequency
+    return frequency
 
 
 def get_joint_pain_antecedents():
@@ -72,25 +72,35 @@ def get_conjunctivitis_antecedents():
 
 
 def get_headache_antecedents():
-    headache_frequency = control.Antecedent(arange(0, 10, .1), 'headache_frequency')
-    headache_intensity = control.Antecedent(arange(0, 10, .1), 'headache_intensity')
+    frequency = control.Antecedent(arange(0, 10, .1), 'headache_frequency')
+    intensity = control.Antecedent(arange(0, 10, .1), 'headache_intensity')
 
-    headache_frequency['low'] = fuzzy.gaussmf(headache_frequency.universe, 0, 1.5)
-    headache_frequency['medium'] = fuzzy.gaussmf(headache_frequency.universe, 5, .75)
-    headache_frequency['high'] = fuzzy.gaussmf(headache_frequency.universe, 10, 1.5)
+    frequency['low'] = fuzzy.gaussmf(frequency.universe, 0, 1.5)
+    frequency['medium'] = fuzzy.gaussmf(frequency.universe, 5, .75)
+    frequency['high'] = fuzzy.gaussmf(frequency.universe, 10, 1.5)
 
-    headache_intensity['low'] = fuzzy.gaussmf(headache_intensity.universe, 0, 1.5)
-    headache_intensity['medium'] = fuzzy.gaussmf(headache_intensity.universe, 5, .75)
-    headache_intensity['high'] = fuzzy.gaussmf(headache_intensity.universe, 10, 1.5)
+    intensity['low'] = fuzzy.gaussmf(intensity.universe, 0, 1.5)
+    intensity['medium'] = fuzzy.gaussmf(intensity.universe, 5, .75)
+    intensity['high'] = fuzzy.gaussmf(intensity.universe, 10, 1.5)
 
-    return headache_frequency, headache_intensity
+    return frequency, intensity
 
 
 def get_itch_antecedents():
-    itch_intensity = control.Antecedent(arange(0, 10, .1), 'itch_intensity')
+    intensity = control.Antecedent(arange(0, 10, .1), 'itch_intensity')
 
-    itch_intensity['mild'] = fuzzy.gaussmf(itch_intensity.universe, 0, 1.5)
-    itch_intensity['moderate'] = fuzzy.gaussmf(itch_intensity.universe, 5, .75)
-    itch_intensity['intense'] = fuzzy.gaussmf(itch_intensity.universe, 10, 1.5)
+    intensity['mild'] = fuzzy.gaussmf(intensity.universe, 0, 1.5)
+    intensity['moderate'] = fuzzy.gaussmf(intensity.universe, 5, .75)
+    intensity['intense'] = fuzzy.gaussmf(intensity.universe, 10, 1.5)
 
-    return itch_intensity
+    return intensity
+
+
+def get_ganglionic_hypertrophy_antecedents():
+    frequency = control.Antecedent(arange(0, 10, .1), 'ganglionic_hypertrophy_frequency')
+
+    frequency['mild'] = fuzzy.gaussmf(frequency.universe, 0, 1.5)
+    frequency['moderate'] = fuzzy.gaussmf(frequency.universe, 5, .75)
+    frequency['intense'] = fuzzy.gaussmf(frequency.universe, 10, 1.5)
+
+    return frequency
