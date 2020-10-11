255c06f8a7125bdda2222a7f3e6a41785091ed92
Rodrigo Ferrerira Guimarães <rodrigofegui@gmail.com>
2020-10-11 20:26:26 -0300

    Edit: Refined joint pain questions

--- src/humanized_questions.py
+++ src/humanized_questions.py
@@ -111,75 +111,55 @@ def ask_about_muscle_pain(medical_record, section):
 
 
 def ask_about_joint_pain(medical_record, section):
-    while True:
-        print('Sentiu dor nas articulações durante a última semana? (sim/não)')
-        resp = input()
+    if wait_valid_answer(
+        'Em algum momento nesta última semana, sentiu alguma dor nas articulações, juntas?',
+        ['sim', 'não']
+    ) == 'não':
+        medical_record.input['joint_pain_freq'] = 0
+        medical_record.input['joint_pain_intensity'] = 0
+        medical_record.input['joint_edema'] = 0
+        medical_record.input['joint_edema_intensity'] = 0
 
-        if resp == 'não':
-            medical_record.input['joint_pain_freq'] = 0
-            medical_record.input['joint_pain_intensity'] = 0
-            medical_record.input['joint_edema'] = 0
-            medical_record.input['joint_edema_intensity'] = 0
+        print(NO_SYMPTOMS_MSG.format(section))
 
-            return medical_record
+        return medical_record
 
-        if resp == 'sim':
-            break
+    print('E, já que teve dores nas juntas,')
+    medical_record.input['joint_pain_freq'] = wait_valid_answer(
+        'Poderia informar se doia com frequência, numa escala de "pouco frequente" a "muito frequente"?',
+        min_value=1,
+        max_value=10,
+        cast_to=int
+    )
 
-    print('Já que teve dores, infelizmente, poderia dizer se doia com frequência?')
+    print('Obrigado! E quanto à intensidade...')
+    medical_record.input['joint_pain_intensity'] = wait_valid_answer(
+        'Doia muito, numa escala de "bastante fraca" a "muito forte"?',
+        min_value=1,
+        max_value=10,
+        cast_to=int
+    )
 
-    while True:
-        print('Numa escala de 1 (pouco frequente) a 10 (muito frequente), por favor')
-        resp = int(input())
+    print('Obrigado!')
+    print('E além das dores, ')
+    medical_record.input['joint_edema'] = wait_valid_answer(
+        'Percebeu algum inchaço nas juntas, numa escala de "não apareceu" a "tinha por todo o corpo"?',
+        min_value=0,
+        max_value=10,
+        cast_to=int
+    )
 
-        if resp >= 1 and resp <= 10:
-            medical_record.input['joint_pain_freq'] = resp
-            break
-        else:
-            print('Infelizmente, esta resposta não pode ser utilizada pela gente...')
-
-    print('Obrigado! Prosseguindo, então')
-    print('E quanto à intensidade, doia muito?')
-
-    while True:
-        print('Numa escala de 1 (bastante fraca) a 10 (muito forte), por favor')
-        resp = int(input())
-
-        if resp >= 1 and resp <= 10:
-            medical_record.input['joint_pain_intensity'] = resp
-            break
-        else:
-            print('Infelizmente, esta resposta não pode ser utilizada pela gente...')
-
-    print('Obrigado! Prosseguindo, então...')
-    print('Além das dores, percebeu algum inchaço nas articulações?')
-
-    while True:
-        print('Numa escala de 0 (não apareceu) a 10 (tinha por todo o corpo), por favor')
-        resp = int(input())
-
-        if resp >= 0 and resp <= 10:
-            medical_record.input['joint_edema'] = resp
-            break
-        else:
-            print('Infelizmente, esta resposta não pode ser utilizada pela gente...')
-
-    if resp == 0:
+    if not medical_record.input['joint_edema']:
         medical_record.input['joint_edema_intensity'] = 0
         return medical_record
 
-    print('Obrigado! Prosseguindo, então...')
-    print('Já que ficaram inchadas, saberia dizer o quão inchada elas ficaram, no geral?')
-
-    while True:
-        print('Numa escala de 1 (pouco inchadas) a 10 (bastante inchadas), por favor')
-        resp = int(input())
-
-        if resp >= 1 and resp <= 10:
-            medical_record.input['joint_edema_intensity'] = resp
-            break
-        else:
-            print('Infelizmente, esta resposta não pode ser utilizada pela gente...')
+    print('Tendo esses inchaços aparecidos,')
+    medical_record.input['joint_edema_intensity'] = wait_valid_answer(
+        'Conseguiria me informar o quão inchado estava, numa escala de "pouco inchadas" a "bastante inchadas"?',
+        min_value=1,
+        max_value=10,
+        cast_to=int
+    )
 
     return medical_record
 
